223cfb69c7c215059aa106e77d436b1676253045
Velicheti Sita <velicheti.sita@lge.com>
2020-09-02 10:08:25 +0000

    [OSE] Support new alsa card master controls in UMI
    :Release Notes:
    Support new alsa card master controls in UMI for mute and volume
    
    :Detailed Notes:
    UMI device-cap.json file and devicecapability class are modified
    such that they support newly changed ALSA master control names to
    set master volume and master mute on hdmi and lineout audio
    
    :Testing Performed:
    Tested on RPI4 rebased on #243 OSE official binary
    Test case QWA-909 QWA-3811 QWA-897 QWA-893
    Test Results are attached to PLAT-112250
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-112250] [OSE] Support new alsa card master controls in UMI
    
    Change-Id: I72fb298ec50e19d54e4391205ef551f43d6cd9bd

--- src/devicecapability.h
+++ src/devicecapability.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2019 LG Electronics, Inc.
+// Copyright (c) 2017-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -44,9 +44,9 @@ private:
     class AudioDefaults
     {
     public:
-        AudioDefaults():card("default"),
-        muteControlName ("\'Master Playback Switch\'"),
-        volumeControlName("\'Master Playback Volume\'") { }
+        AudioDefaults():card("hw:Headphones"),
+        muteControlName ("\'Headphone Playback Switch\'"),
+        volumeControlName("\'Headphone Playback Volume\'") { }
         ~AudioDefaults(){}
         std::string card;
         std::string muteControlName;
